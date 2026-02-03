9eeb21c2f7beda02f9713b5a8693e15209c3bbd4
biansuv <biansuv@gmail.com>
2026-02-03 07:26:44 +0530

    Update header.js
    Header polished

--- header.js
+++ header.js
@@ -3,12 +3,26 @@
 ================================ */
 
 function renderHeader(titleText) {
+    const user = JSON.parse(localStorage.getItem("loggedUser"));
+
     const header = document.createElement("div");
     header.className = "top-bar";
 
     header.innerHTML = `
-        <h2>${titleText}</h2>
-        <button class="logout-btn" onclick="logout()">Logout</button>
+        <div class="header-left">
+            <h2>${titleText}</h2>
+        </div>
+
+        <div class="header-right">
+            ${
+                user
+                ? `<span class="user-info">
+                        👤 ${user.name} (${user.role})
+                   </span>`
+                : ""
+            }
+            <button class="logout-btn" onclick="logout()">Logout</button>
+        </div>
     `;
 
     document.body.prepend(header);
